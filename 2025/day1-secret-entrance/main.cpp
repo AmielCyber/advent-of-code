577c57174ee2b15b1c5cab8dfe30062f2486d22a
Amiel Nava <amieln@icloud.com>
2025-12-06 16:47:36 -0800

    Solution

--- 2025/day1-secret-entrance/main.cpp
+++ 2025/day1-secret-entrance/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
-int main() {
-  std::cout << "Hello world";
+#include <fstream>
+#include "Dial.h"
+
+void parse_rotations(std::ifstream& file, Dial& dial) {
+  std::string line;
+  int rotations = 0;
+  while (std::getline(file, line)) {
+    const char rotation = line[0];
+    try {
+      rotations = std::stoi(line.substr(1));
+    }catch (std::invalid_argument& e) {
+      std::cout << e.what() << '\n';
+      std::cout << "Invalid input: \"" << line << '\"' << '\n';
+    }
+    if (rotation == 'R') {
+      dial.right_rotation(rotations);
+    }
+    if (rotation == 'L') {
+      dial.left_rotation(rotations);
+    }
+  }
+}
+
+int main(int _, char *argv[]) {
+  Dial dial;
+  const std::string file_name{argv[1]};
+  std::ifstream file(file_name);
+  if (!file.is_open()) {
+    std::cerr << "Could not open file \"" << file_name <<'"'<< '\n';
+    return 1;
+  }
+
+  try {
+    parse_rotations(file, dial);
+  }catch (...) {
+    file.close();
+    throw;
+  }
+  file.close();
+  std::cout << dial.get_zero_point_count() << '\n';
   return 0;
 }
