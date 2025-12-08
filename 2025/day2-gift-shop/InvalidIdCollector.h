2e702ef65f55c82833a18bd33f5535c82b92cbd6
Amiel Nava <amieln@icloud.com>
2025-12-07 17:22:33 -0800

    Part 1 done

---
+++ 2025/day2-gift-shop/InvalidIdCollector.h
@@ -0,0 +1,22 @@
+//
+// Created by Amiel Nava on 12/7/25.
+//
+
+#ifndef DAY2_GIFT_SHOP_INVALID_ID_COLLECTOR_H
+#define DAY2_GIFT_SHOP_INVALID_ID_COLLECTOR_H
+#include <vector>
+
+
+class InvalidIdCollector {
+public:
+  InvalidIdCollector() = default;
+  void add_invalid_ids(long low, long high);
+  std::vector<long long> get_invalid_ids();
+private:
+  std::vector<long long> invalidIds;
+
+  static bool is_symmetric(long num);
+};
+
+
+#endif //DAY2_GIFT_SHOP_INVALID_ID_COLLECTOR_H
---
+++ 2025/day2-gift-shop/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <numeric>
+#include <vector>
+#include <fstream>
+#include <sstream>
+#include <string>
+#include "InvalidIdCollector.h"
+
+void parse_ids(std::ifstream& file, InvalidIdCollector& invalid_id_collector) {
+  std::string line;
+  std::getline(file, line); // One line
+
+  std::stringstream ss(line);
+  std::string range; // xxx-yyy
+  while (std::getline(ss, range, ',')) {
+    if (const size_t pos = range.find_first_of('-'); pos != std::string::npos) {
+      const long low = std::stol(range.substr(0, pos)); // xxx
+      const long high = std::stol(range.substr(pos+1));       // yyy
+      invalid_id_collector.add_invalid_ids(low, high);
+    }
+  }
+}
+
+void parse_ids(const std::string& file_name, InvalidIdCollector& invalid_id_collector) {
+  std::ifstream file(file_name);
+  if (!file.is_open()) {
+    std::cerr << "Could not open file \"" << file_name <<'"'<< '\n';
+    return;
+  }
+  try {
+    parse_ids(file, invalid_id_collector);
+  }catch (...) {
+    file.close();
+    throw;
+  }
+  file.close();
+}
+
+int main() {
+  InvalidIdCollector invalid_id_collector;
+  parse_ids("./input.txt", invalid_id_collector);
+
+  std::vector<long long> result = invalid_id_collector.get_invalid_ids();
+  std::cout << std::accumulate(result.begin(), result.end(), 0LL) << std::endl;
+}
