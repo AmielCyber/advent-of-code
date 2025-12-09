9436e10070ff5b8514cd54aa50645af87167b6c1
Amiel Nava <amieln@icloud.com>
2025-12-09 12:01:25 -0800

    Part 2 Done 🙌

--- 2025/day3-lobby/BatteryBank.cpp
+++ 2025/day3-lobby/BatteryBank.cpp
@@ -27,6 +27,45 @@ void BatteryBank::add_optimal_batteries(const std::string &bank) {
   battery_jolts.push_back(jolts);
 }
 
-std::vector<int> BatteryBank::get_optimal_batteries() {
+/**
+ *  TIME: O(NK^2)
+ *  Space: O(K)
+ */
+void BatteryBank::add_optimal_batteries_part2(const std::string &bank, const int k) {
+  if (bank.empty()) {
+    return;
+  }
+  std::vector<char> optimal(k, '0');
+  int curr = 0;
+  for (int i = 0; i < bank.size(); i++) {
+    if (optimal[curr] < bank[i]) {
+      // Find pointer to the left where optimal[curr] >= bank[i] and optimal[curr+1] < bank[i]
+      // AKA Insertion Sort
+      for (;curr > -1 && optimal[curr] < bank[i] && optimal.size()-curr <= bank.size() - i ; --curr) {
+        optimal[curr] = '0';
+      }
+      curr++;
+      optimal[curr] = bank[i];
+    }else if (curr != optimal.size() - 1) {
+      // Add value to any zeros to the right of current
+      curr = curr + 1;
+      optimal[curr] = bank[i];
+    }
+  }
+  add_char_vector_jolts_to_battery_vector_jolts(optimal);
+}
+
+std::vector<long > BatteryBank::get_optimal_batteries() {
   return battery_jolts;
+}
+
+void BatteryBank::add_char_vector_jolts_to_battery_vector_jolts(const std::vector<char> &battery) {
+  long jolts = 0;
+  for (int i = 0; i < battery.size() && battery[i] != '0'; i++) {
+    jolts += battery[i] - '0';
+    if (i < battery.size() - 1) {
+      jolts *= 10;
+    }
+  }
+  battery_jolts.push_back(jolts);
 }
